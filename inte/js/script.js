4230ace6e9614d8fcd38683a82c50d8c97a93dc0
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-05-26 18:16:49 +0200

    :art: page article

--- inte/js/script.js
+++ inte/js/script.js
@@ -1,19 +1,36 @@
 $(function(){
 
-    var myScroll = 0,
-        header = $('#header'),
+    var myScroll = 0, scrollDir = 0, lastScrollTop = 0,
+        header = $('#header'), headerHeight = header.innerHeight(),
         mainContent = $('#mainContent'),
         readIndicator = $('#readIndicator'),
         docHeight = $(document).height(),
         windowHeight = $(window).height(),
-        buttons = $('.btn');
+        buttons = $('.btn'),
+        main = $('#main'),
+        contentHeader = $('#contentHeader'),
+        postSidebar = $('#postSidebar'),
+        postSidebarTop = 0;
 
     /**** VARIABLES ****/
 
 
     /**** INIT ****/
 
-    if(buttons.length){
+    function detectScrollDir(){
+        if(myScroll > lastScrollTop){
+            scrollDir = -1;
+        }else if(myScroll < lastScrollTop){
+            scrollDir = 1;
+        }else{
+            scrollDir = 0;
+        }
+        lastScrollTop = myScroll;
+    }
+
+
+
+    function setButtons(){
         var i = 0, nbButtons = buttons.length, letterArray = [],
             textBtn = '', j = 0, nbLetter = 0, newHtmlBtn = '',
             delay = 0;
@@ -33,27 +50,76 @@ $(function(){
         }
     }
 
+    function setSidebarScroll(){
+        var sidebarMargin = 50, sidebarHeight = postSidebar.innerHeight(),
+            contentTop = mainContent.offset().top, contentHeight = mainContent.innerHeight();
+
+        if(sidebarHeight < contentHeight + sidebarMargin*2){
+            if(myScroll >= postSidebarTop - headerHeight - sidebarMargin){
+                postSidebar.css({top: headerHeight + sidebarMargin, width: postSidebar.innerWidth()}).removeClass('fixedBot').addClass('fixed');
+
+                if(myScroll + sidebarHeight + headerHeight + sidebarMargin > contentTop + contentHeight - sidebarMargin){
+                    postSidebar.css({top: contentHeight - sidebarMargin - sidebarHeight + 20}).removeClass('fixed').addClass('fixedBot');
+                }
+
+                if(myScroll > (contentHeight+contentTop)/2){
+                    postSidebar.find('li').eq(0).find('.img').addClass('off');
+                    postSidebar.find('li').eq(1).find('.img').addClass('on');
+                }else{
+                    postSidebar.find('li').eq(0).find('.img').removeClass('off');
+                    postSidebar.find('li').eq(1).find('.img').removeClass('on');
+                }
+            }else{
+                postSidebar.css({top: 0}).removeClass('fixed fixedBot');
+            }
+        }
+    }
+
+    if(buttons.length){
+        setButtons();
+    }
+
 
     $(document).scroll(function(){
         myScroll = $(document).scrollTop();
+        detectScrollDir();
 
         if(mainContent.length){
-            myScroll > mainContent.offset().top - header.innerHeight() ? header.addClass('fixed') : header.removeClass('fixed');
+            myScroll > mainContent.offset().top - headerHeight - 40 ? header.addClass('fixed') : header.removeClass('fixed');
+            if(header.hasClass('fixed')){
+                scrollDir < 0 ? header.addClass('on') : header.removeClass('on');
+            }
         }
 
         if(readIndicator.length){
             var readingPercent = myScroll/(docHeight-windowHeight);
             TweenMax.set(readIndicator, {scaleX: readingPercent});
         }
+
+        if(postSidebar.length && mainContent.length){
+            setSidebarScroll();
+        }
     });
 
     $(window).resize(function(){
         docHeight = $(document).height();
         windowHeight = $(window).height();
+
+        if(main.length && contentHeader.length){
+            main.css('marginTop', contentHeader.innerHeight());
+        }
 	});
 
 	$(window).load(function(){
         docHeight = $(document).height();
+
+        if(main.length && contentHeader.length){
+            main.css('marginTop', contentHeader.innerHeight());
+        }
+
+        if(postSidebar.length){
+            postSidebarTop = postSidebar.offset().top;
+        }
 	});
 
 });
