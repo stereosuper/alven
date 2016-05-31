9ea299ba5cf72095b304b46442f7bb2728dbbb70
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-05-31 16:54:36 +0200

    :art: responsive + drag n drop + hover buttons

--- inte/js/script.js
+++ inte/js/script.js
@@ -1,17 +1,21 @@
 $(function(){
 
-    var myScroll = 0, scrollDir = 0, lastScrollTop = 0,
+    var htmlTag = $('html'),
+        myScroll = 0, scrollDir = 0, lastScrollTop = 0,
         header = $('#header'), headerHeight = header.innerHeight(),
         mainContent = $('#mainContent'),
         readIndicator = $('#readIndicator'),
         docHeight = $(document).height(),
         windowHeight = $(window).height(),
+        windowWidth = $(window).width(),
         buttons = $('.btn'),
         buttonsInvert = $('.btn-invert'),
         main = $('#main'),
         contentHeader = $('#contentHeader'),
         postSidebar = $('#postSidebar'),
-        postSidebarTop = 0 /*postSidebarPos = 0,*/;
+        postSidebarTop = 0 /*postSidebarPos = 0,*/, postSidebarWidth = 0,
+        spotlightPost = $('#spotlightPost'),
+        spotlightDrag = false;
 
     /**** VARIABLES ****/
 
@@ -48,6 +52,48 @@ $(function(){
         }
     }
 
+    function setSpotlightPost(){
+        var posts = spotlightPost.find('.spotlight-post'),
+            spotlightWidth = spotlightPost.find('.container').innerWidth();
+
+        function detectVisiblePosts(){
+            var i = 0, nbPosts = posts.length, postWidth = posts.eq(0).innerWidth();
+            for(i; i<nbPosts; i++){
+                var postPos = posts.eq(i).offset().left + postWidth;
+                if(postPos > windowWidth || postPos < postWidth){
+                    posts.eq(i).addClass('off');
+                }else{
+                    posts.eq(i).removeClass('off');
+                }
+            }
+        }
+
+        detectVisiblePosts();
+
+        if(windowWidth < spotlightWidth){
+            if(!spotlightDrag){
+                spotlightDrag = Draggable.create( '#spotlightDrag', {
+                    type: 'x',
+                    bounds: spotlightPost,
+                    cursor: 'grab',
+                    onDrag: detectVisiblePosts,
+                    onDragStart: function(){
+                        spotlightPost.find('.container').addClass('grabbing');
+                    },
+                    onDragEnd: function(){
+                        spotlightPost.find('.container').removeClass('grabbing');
+                    }
+                } );
+            }else{
+                spotlightDrag[0].enable();
+            }
+        }else{
+            if(spotlightDrag){
+                spotlightDrag[0].disable();
+            }
+        }
+    }
+
     function animTxt(splitText){
         splitText.split({type:'words'});
         TweenMax.staggerFrom(splitText.words, 0.3, {ease:Expo.easeInOut, opacity:0, y:100}, 0.03);
@@ -60,9 +106,9 @@ $(function(){
         if(sidebarHeight < contentHeight + sidebarMargin*2 && sidebarHeight + headerHeight + sidebarMargin < windowHeight){
             if(myScroll >= postSidebarTop - headerHeight - sidebarMargin){
                 if(myScroll + sidebarHeight + headerHeight + sidebarMargin > contentTop + contentHeight - sidebarMargin){
-                    postSidebar.css({top: contentHeight - sidebarMargin - sidebarHeight + 20}).removeClass('fixed').addClass('fixedBot');
+                    postSidebar.css({top: contentHeight - sidebarMargin - sidebarHeight + 20, width: postSidebarWidth}).removeClass('fixed').addClass('fixedBot');
                 }else{
-                    postSidebar.css({top: headerHeight + sidebarMargin, width: postSidebar.innerWidth()}).removeClass('fixedBot').addClass('fixed');
+                    postSidebar.css({top: headerHeight + sidebarMargin, width: postSidebarWidth}).removeClass('fixedBot').addClass('fixed');
                 }
 
                 if(myScroll > (contentHeight+contentTop)/2){
@@ -73,7 +119,7 @@ $(function(){
                     postSidebar.find('li').eq(1).find('.img').removeClass('on');
                 }
             }else{
-                postSidebar.css({top: 0}).removeClass('fixed fixedBot');
+                postSidebar.css({top: 0, width: postSidebarWidth}).removeClass('fixed fixedBot');
             }
             /*if(windowHeight >= sidebarHeight*2 + headerHeight){
                 if(myScroll >= contentTop && myScroll >= windowHeight/2 - sidebarHeight/2){
@@ -91,11 +137,18 @@ $(function(){
         }
     }
 
+
+
+    isMobile.any ? htmlTag.addClass('is-mobile') : htmlTag.addClass('is-desktop');
+
     if(buttons.length){
         setButtons(buttons);
     }
     if(buttonsInvert.length){
         setButtons(buttonsInvert);
+    }
+    if(spotlightPost.length){
+        setSpotlightPost();
     }
 
     $('input').on('change', function(){
@@ -121,14 +174,14 @@ $(function(){
             }
         }
 
-        if(contentHeader.length){
+        if(contentHeader.length && !isMobile.any){
             if(contentHeader.find('.img').length){
                 TweenMax.set(contentHeader.find('.img'), {y: '-'+myScroll/40+'%'});
                 TweenMax.set(contentHeader.find('h1'), {y: '-'+myScroll/4+'%'});
             }
         }
 
-        if(postSidebar.length && mainContent.length){
+        if(postSidebar.length && mainContent.length && !isMobile.any){
             setSidebarScroll();
         }
     });
@@ -136,6 +189,7 @@ $(function(){
     $(window).resize(function(){
         docHeight = $(document).height();
         windowHeight = $(window).height();
+        windowWidth = $(window).width();
 
         if(main.length && contentHeader.length){
             main.css('marginTop', contentHeader.innerHeight());
@@ -143,6 +197,11 @@ $(function(){
 
         if(postSidebar.length){
             postSidebar.css({top: 0, width: '20%'}).removeClass('fixed fixedBot');
+            postSidebarWidth = postSidebar.innerWidth() - 1;
+        }
+
+        if(spotlightPost.length){
+            setSpotlightPost();
         }
 	});
 
@@ -162,6 +221,7 @@ $(function(){
 
         if(postSidebar.length){
             postSidebarTop = postSidebar.offset().top;
+            postSidebarWidth = postSidebar.innerWidth() - 1;
             //postSidebarPos = postSidebar.position().top;
         }
 	});
