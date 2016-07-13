1c163ba8cf7a5b383e80615d5e2419e2fe35d0ee
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-07-13 18:58:04 +0200

    :bug: fix figure size + z-index gallery + change contact texts

--- wp/wp-content/themes/alven/includes/contact.php
+++ wp/wp-content/themes/alven/includes/contact.php
@@ -12,14 +12,14 @@
                 <?php } ?>
                 <form action='<?php the_permalink(); ?>#contact' method='post' enctype='multipart/form-data' class='align-left form-to-open <?php if($status === "error") echo "form-open-error"; ?>'>
                     <fieldset>
-                        <legend class='active'>Let's <span>Introduce yourself</span></legend>
+                        <legend class='active'>Please <span>Introduce yourself</span></legend>
                         <section class='form-section <?php if($errorFirstname || $errorLastname || $errorEmail) echo "invalid"; ?>'>
                             <div>
                                 <input type='text' name='firstname1' id='firstname1' required class='form-elt <?php if($errorFirstname) echo "invalid"; ?>' value='<?php echo $firstname; ?>'>
-                                <label for='firstname1'>Your firstname</label>
+                                <label for='firstname1'>Your first name</label>
                             </div><div>
                                 <input type='text' name='lastname1' id='lastname1' required class='form-elt <?php if($errorLastname) echo "invalid"; ?>' value='<?php echo $lastname; ?>'>
-                                <label for='lastname1'>Your lastname</label>
+                                <label for='lastname1'>Your last name</label>
                             </div><div class='large'>
                                 <input type='email' name='email1' id='email1' required class='form-elt <?php if($errorEmail) echo "invalid"; ?>' value='<?php echo $email; ?>'>
                                 <label for='email1'>Your email</label>
@@ -27,28 +27,28 @@
                         </section>
                     </fieldset>
                     <fieldset>
-                        <legend>Send us <span>Your amazing pitch</span></legend>
+                        <legend>Send us <span>Your pitch deck</span></legend>
                         <section class='form-section <?php if($errorFile || $errorUrl || $errorMsg) echo "invalid"; ?>'>
                             <div class='full has-desc'>
                                 <input type='hidden' name='MAX_FILE_SIZE' value='20971520'>
                                 <input type='file' name='pitchfile' id='pitchfile'><label for='pitchfile'>
                                     Upload your file
                                 </label><span class='form-desc'>
-                                    a lightweight .pdf, .key, .ptt, .pptx
+                                    .pdf, .key, .ptt, .pptx
                                 </span>
                             </div>
                             <span class='form-title'>Or</span>
                             <div class='full has-desc'>
                                 <input type='url' name='pitchurl' id='pitchurl' class='form-elt <?php if($errorUrl) echo "invalid"; ?>'><label for='pitchurl' value='<?php echo $url; ?>'>
-                                    Send us your link
+                                    Insert a link to download your deck
                                 </label><span class='form-desc'>
-                                    a web page which describe your product or service
+                                    insert a link for us to download your deck - write full url with http://
                                 </span>
                             </div>
                             <span class='form-title'>And</span>
                             <div class='full'>
                                 <textarea name='pitchtext' id='pitchtext' required class='form-elt <?php if($errorMsg) echo "invalid"; ?>'><?php echo $msg; ?></textarea>
-                                <label for='ptichtext'>Write us a few word there about what you expect from us</label>
+                                <label for='ptichtext'>Please give us additional context about your company in a few words, useful links, how you heard about Alven Capital...</label>
                             </div>
                         </section>
                     </fieldset>
@@ -61,17 +61,17 @@
             <?php if($status2 === 'success'){ ?>
                 <p class='form-success'>Your message has been sent!<br> Thank you, we will be back to you shortly.</p>
             <?php }else{ ?>
-                <button class='btn open-form'>General questions</button>
+                <button class='btn open-form'>Contact us</button>
                 <form action='<?php the_permalink(); ?>#contact' method='post' class='align-left form-to-open <?php if($status2 === "error") echo "form-open-error"; ?>'>
                     <fieldset>
-                        <legend class='active'>Let's <span>Introduce yourself</span></legend>
+                        <legend class='active'>Please <span>Introduce yourself</span></legend>
                         <section class='form-section <?php if($errorFirstname2 || $errorLastname2 || $errorEmail2) echo "invalid"; ?>'>
                             <div>
                                 <input type='text' name='firstname2' id='firstname2' required class='form-elt <?php if($errorFirstname2) echo "invalid"; ?>' value='<?php echo $firstname2; ?>'>
-                                <label for='firstname2'>Your firstname</label>
+                                <label for='firstname2'>Your first name</label>
                             </div><div>
                                 <input type='text' name='lastname2' id='lastname2' required class='form-elt <?php if($errorLastname2) echo "invalid"; ?>' value='<?php echo $lastname2; ?>'>
-                                <label for='lastname2'>Your lastname</label>
+                                <label for='lastname2'>Your last name</label>
                             </div><div class='large'>
                                 <input type='email' name='email2' id='email2' required class='form-elt <?php if($errorEmail2) echo "invalid"; ?>' value='<?php echo $email2; ?>'>
                                 <label for='email2'>Your email</label>
@@ -79,11 +79,11 @@
                         </section>
                     </fieldset>
                     <fieldset>
-                        <legend>We're ready to read: <span>What would you like to know?</span></legend>
+                        <legend>Your message <span>How can we help you? </span></legend>
                         <section class='form-section <?php if($errorMsg2) echo "invalid"; ?>'>
                             <div class='full'>
                                 <textarea id='msg' name='msg' required class='form-elt <?php if($errorMsg2) echo "invalid"; ?>'><?php echo $msg2; ?></textarea>
-                                <label for='msg'>Write us a few word there about what you expect from us</label>
+                                <label for='msg'>Write your inquiry here</label>
                             </div>
                         </section>
                     </fieldset>
