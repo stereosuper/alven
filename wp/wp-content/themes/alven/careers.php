16c635dd5e7a16eb567c70914e57d63cde4c2523
ElisabethStereosuper <elisabeth@stereosuper.fr>
2018-11-29 16:55:44 +0100

    :art: jobs markup

--- wp/wp-content/themes/alven/careers.php
+++ wp/wp-content/themes/alven/careers.php
@@ -127,17 +127,16 @@ get_header();
         <main role='main' id='main'>
             <section class='startups-jobs'>
                 <div class='container flex-container'>
-                    <div class='col-3 no-padding'>
+                    <div class='col-3 no-padding-left'>
                         <h2 class='job-sidebar-title'>Start-up jobs</h2>
                         <p>Join the alven Family</p>
-                        <aside class="fields-area" role="complementary" aria-label="<?php esc_attr_e( 'Careers filters', 'alven' ); ?>">
-                            <form role='search' method='get' action='<?php echo get_permalink(); ?>' class='form-search'>
-                                <input type='search' name='s' value='<?php the_search_query(); ?>' id='search'>
-                                <button type='submit' class='btn-search btn-no-text'>Explore</button>
-                                <label for='search'>type some keywords</label>
-                                <!--<button type='submit' class='btn-search btn-no-text'>Explore</button>-->
+                        <form role='search' method='get' action='<?php echo get_permalink(); ?>' class='form-search' aria-label="<?php esc_attr_e( 'Careers filters', 'alven' ); ?>">
+                            <input type='search' name='s' value='<?php the_search_query(); ?>' id='search'>
+                            <button type='submit' class='btn-search btn-no-text'>Explore</button>
+                            <label for='search'>type some keywords</label>
                            
-                                <!-- Locations with number of jobs -->
+                            <!-- Locations with number of jobs -->
+                            <div class='select'>
                                 <select name='location'>
                                     <option value=''>All Locations</option>
                                     <?php
@@ -150,7 +149,9 @@ get_header();
                                         }
                                     ?>
                                 </select>
-                                <!-- Sartups with number of jobs -->
+                            </div>
+                            <!-- Sartups with number of jobs -->
+                            <div class='select'>
                                 <select name='company'>
                                     <option value=''>All Companies</option>
                                     <?php
@@ -163,7 +164,9 @@ get_header();
                                         }
                                     ?>
                                 </select>
-                                <!-- Functions -->
+                            </div>
+                            <!-- Functions -->
+                            <div class='select'>
                                 <select name='function'>
                                     <option value=''>All Functions</option>
                                     <?php
@@ -176,7 +179,9 @@ get_header();
                                         }
                                     ?>
                                 </select>
-                                <!-- Sectors -->
+                            </div>
+                            <!-- Sectors -->
+                            <div class='select'>
                                 <select name='sector'>
                                     <option value=''>All Sectors</option>
                                     <?php
@@ -189,52 +194,54 @@ get_header();
                                         }
                                     ?>
                                 </select>
-                                <input type="submit" id="searchsubmit" value="Search" />
-                            </form>
-                        </aside>
-                    </div>
-                    <div class='col-8 no-padding'>
-                        <div class=''>
-                            <?php 
-                                if( $is_details ):
-                                    echo 'contenu de loffre';
-                                    var_dump(get_field('job_location', get_the_ID()));
-                                else:
-                                    echo '<div class="list-jobs flex-container">';
-                                    foreach ($jobs_extended as $key => $job) {
-                                        $article = '<a href="'.esc_url( get_permalink( $job->ID ) ).'" class="job no-padding">';
-                                            $article .= '<div class="align-center"><img src="'.$job->from['logo'].'" alt="'.$job->from['name'].'"></div>';
-                                            $article .= '<div><p class="job-title">'.$job->post_title.'</p><p class="job-location">'.$job->location.'</p></div>';
-                                        $article .= '</a>';
-                                        echo $article;
-                                    }
-                                    echo "</div>";
+                            </div>
 
-                                    echo "<div class='job-paginate align-center'>";
-                                        $big = 999999999; // need an unlikely integer
-                                        echo paginate_links( array(
-                                            'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-                                            'format'    => '?paged=%#%',
-                                            'current'   => max( 1, get_query_var('paged') ),
-                                            'total'     => $jobs->max_num_pages,
-                                            'prev_text' => '',
-                                            'next_text' => ''
-                                        ) );
-                                    echo "</div>";
-                                endif;
-                            ?>
-                        </div>
+                            <button type="submit" id="searchsubmit" class="btn">Search</button>
+                        </form>
+                    </div>
+
+                    <div class='col-8 no-padding-right'>
+                        <?php 
+                            if( $is_details ):
+                                echo 'contenu de loffre';
+                                var_dump(get_field('job_location', get_the_ID()));
+                            else:
+                                echo '<div class="list-jobs flex-container">';
+                                foreach ($jobs_extended as $key => $job) {
+                                    $article = '<a href="'.esc_url( get_permalink( $job->ID ) ).'" class="job no-padding">';
+                                    $article .= '<div class="align-center"><img src="'.$job->from['logo'].'" alt="'.$job->from['name'].'"></div>';
+                                    $article .= '<div><p class="job-title">'.$job->post_title.'</p><p class="job-location">'.$job->location.'</p></div>';
+                                    $article .= '</a>';
+                                    echo $article;
+                                }
+                                echo "</div>";
+
+                                echo "<div class='job-paginate align-center'>";
+                                $big = 999999999; // need an unlikely integer
+                                echo paginate_links( array(
+                                    'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                                    'format'    => '?paged=%#%',
+                                    'current'   => max( 1, get_query_var('paged') ),
+                                    'total'     => $jobs->max_num_pages,
+                                    'prev_text' => '',
+                                    'next_text' => ''
+                                ) );
+                                echo "</div>";
+                            endif;
+                        ?>
                         <?php wp_reset_query(); ?>
                     </div>
                 </div>
             </section>
+
             <section class='alven-jobs'>
                 <div class='container flex-container'>
-                    <div class='col-3 no-padding'>
+                    <div class='col-3 no-padding-left'>
                         <h2 class='job-sidebar-title'>Alven jobs</h2>
                         <p>Join the alven team</p>
                     </div>
-                    <div class='col-8 no-padding'>
+
+                    <div class='col-8 no-padding-right'>
                         <div class="list-jobs-alven flex-container">
                             <a href="" class="job-alven">
                                 <p class="job-title-alven">Business development assistant</p>
@@ -252,6 +259,7 @@ get_header();
                     </div>
                 </div>
             </section>
+
             <section class=''>
             </section>
         </main>
