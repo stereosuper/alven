54058ef5af20e3343599f328502cf6a6ebba22d0
ElisabethStereosuper <elisabeth@stereosuper.fr>
2018-11-29 15:34:18 +0100

    :art: alven jobs

--- wp/wp-content/themes/alven/careers.php
+++ wp/wp-content/themes/alven/careers.php
@@ -93,7 +93,7 @@ get_header();
             <section class='startups-jobs'>
                 <div class='container flex-container'>
                     <div class='col-3 no-padding'>
-                        <h2>Start-up jobs</h2>
+                        <h2 class='job-sidebar-title'>Start-up jobs</h2>
                         <p>Join the alven Family</p>
                         <aside class="fields-area" role="complementary" aria-label="<?php esc_attr_e( 'Careers filters', 'alven' ); ?>">
                             <form role='search' method='get' action='<?php echo home_url('/'); ?>' class='form-search'>
@@ -140,50 +140,69 @@ get_header();
                         </aside>
                     </div>
                     <div class='col-8 no-padding'>
-                        <div class=''>
-                            <?php 
-                                if( $is_details ):
-                                    echo 'contenu de loffre';
-                                    var_dump(get_field('job_location', get_the_ID()));
-                                else:
-                                    echo '<div class="list-jobs flex-container">';
-                                    foreach ($jobs_extended as $key => $job) {
-                                        /*echo '<pre>';
-                                        var_dump($job);
-                                        echo '</pre>';*/
-                                        $article = '<a href="'.esc_url( get_permalink( $job->ID ) ).'" class="job no-padding">';
-                                            $article .= '<div><img src="'.$job->from['logo'].'" alt="'.$job->from['name'].'"></div>';
-                                            $article .= '<div><p class="job-title">'.$job->post_title.'</p><p class="job-location">'.$job->location.'</p></div>';
-                                        $article .= '</a>';
-                                        echo $article;
-                                    }
-                                    echo "</div>";
+                        <?php 
+                            if( $is_details ):
+                                echo 'contenu de loffre';
+                                var_dump(get_field('job_location', get_the_ID()));
+                            else:
+                                echo '<div class="list-jobs flex-container">';
+                                foreach ($jobs_extended as $key => $job) {
+                                    /*echo '<pre>';
+                                    var_dump($job);
+                                    echo '</pre>';*/
+                                    $article = '<a href="'.esc_url( get_permalink( $job->ID ) ).'" class="job">';
+                                        $article .= '<div><img src="'.$job->from['logo'].'" alt="'.$job->from['name'].'"></div>';
+                                        $article .= '<div><p class="job-title">'.$job->post_title.'</p><p class="job-location">'.$job->location.'</p></div>';
+                                    $article .= '</a>';
+                                    echo $article;
+                                }
+                                echo "</div>";
 
-                                    echo "<div class='job-paginate align-center'>";
-                                        $big = 999999999; // need an unlikely integer
-                                        echo paginate_links( array(
-                                            'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-                                            'format'    => '?paged=%#%',
-                                            'current'   => max( 1, get_query_var('paged') ),
-                                            'total'     => $jobs->max_num_pages,
-                                            'prev_text' => '',
-                                            'next_text' => ''
-                                        ) );
-                                    echo "</div>";
-                                endif;
-                            ?>
-                        </div>
+                                echo "<div class='job-paginate align-center'>";
+                                    $big = 999999999; // need an unlikely integer
+                                    echo paginate_links( array(
+                                        'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                                        'format'    => '?paged=%#%',
+                                        'current'   => max( 1, get_query_var('paged') ),
+                                        'total'     => $jobs->max_num_pages,
+                                        'prev_text' => '',
+                                        'next_text' => ''
+                                    ) );
+                                echo "</div>";
+                            endif;
+                        ?>
                         <?php wp_reset_query(); ?>
                     </div>
                 </div>
             </section>
             <section class='alven-jobs'>
-                <div class='container'>
+                <div class='container flex-container'>
                     <div class='col-3 no-padding'>
-                        <h2>Alven jobs</h2>
+                        <h2 class='job-sidebar-title'>Alven jobs</h2>
                         <p>Join the alven team</p>
                     </div>
-                    <div class='col-8 no-padding'></div>
+                    <div class='col-8 no-padding'>
+                        <div class="list-jobs-alven flex-container">
+                            <a href="" class="job-alven">
+                                <div>
+                                    <p class="job-title-alven">Business development assistant</p>
+                                    <p class="job-location-alven">Paris, France</p>
+                                </div>
+                            </a>
+                            <a href="" class="job-alven">
+                                <div>
+                                    <p class="job-title-alven">Business development assistant</p>
+                                    <p class="job-location-alven">Paris, France</p>
+                                </div>
+                            </a>
+                            <a href="" class="job-alven">
+                                <div>
+                                    <p class="job-title-alven">Business development assistant</p>
+                                    <p class="job-location-alven">Paris, France</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </section>
             <section class=''>
