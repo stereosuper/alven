312a7072c50799081dbd8585568df461beab72c0
ElisabethStereosuper <elisabeth@stereosuper.fr>
2018-11-30 18:16:42 +0100

    :art: single job

--- wp/wp-content/themes/alven/careers.php
+++ wp/wp-content/themes/alven/careers.php
@@ -239,32 +239,32 @@ get_header();
                             if( $is_details ):
                                 echo '<div class="details-job flex-container">';
                                     echo '<div class="details-job-main">';
-                                        $details_header  = '<header>';
-                                        $details_header .= '<div><p>'.__('Job Type','alven').'</p><p>Permanent Contract</p></div>';
-                                        $details_header .= '<div><p>'.__('Location','alven').'</p><p>Paris</p></div>';
+                                        $details_header  = '<header class="flex-container details-job-header">';
+                                        $details_header .= '<div><p class="title-small">'.__('Job Type','alven').'</p><p>Permanent Contract</p></div>';
+                                        $details_header .= '<div><p class="title-small">'.__('Location','alven').'</p><p>Paris</p></div>';
                                         $details_header .= '</header>';
                                         echo $details_header;
 
                                         $details_section = '<section>';
                                         if( $details ):
                                             if( $details['job_details_about'] ):
-                                                $details_section .= '<h2>'.__('About','alven').'&nbsp'.$company_datas['name'].'</h2>';
+                                                $details_section .= '<h2 class="job-single-title">'.__('About','alven').'&nbsp'.$company_datas['name'].'</h2>';
                                                 $details_section .= $details['job_details_about'];
                                             endif;
                                             if( $details['job_details_desc'] ):
-                                                $details_section .= '<h2>'.__('Job description','alven').'</h2>';
+                                                $details_section .= '<h2 class="job-single-title">'.__('Job description','alven').'</h2>';
                                                 $details_section .= $details['job_details_desc'];
                                             endif;
                                             if( $details['job_details_profil'] ):
-                                                $details_section .= '<h2>'.__('Profile','alven').'</h2>';
+                                                $details_section .= '<h2 class="job-single-title">'.__('Profile','alven').'</h2>';
                                                 $details_section .= $details['job_details_profil'];
                                             endif;
                                             if( $details['job_details_why'] ):
-                                                $details_section .= '<h2>'.__('Why you should join us','alven').'</h2>';
+                                                $details_section .= '<h2 class="job-single-title">'.__('Why you should join us','alven').'</h2>';
                                                 $details_section .= $details['job_details_why'];
                                             endif;
                                             if( $details['job_details_process'] ):
-                                                $details_section .= '<h2>'.__('Hiring Process','alven').'</h2>';
+                                                $details_section .= '<h2 class="job-single-title">'.__('Hiring Process','alven').'</h2>';
                                                 $details_section .= $details['job_details_process'];
                                             endif;
                                         endif;
@@ -273,12 +273,12 @@ get_header();
                                     echo '</div>';
                                     echo '<div class="details-job-sidebar">';
                                         if( $urlapply ):
-                                            echo '<a href="'.$urlapply.'" alt="'.__('Apply','alven').'" tarrget="_blank" rel="noopener noreferrer">'.__('Apply','alven').'</a>';
+                                            echo '<a href="'.$urlapply.'" alt="'.__('Apply','alven').'" tarrget="_blank" rel="noopener noreferrer" class="btn">'.__('Apply','alven').'</a>';
                                         endif;
                                         $job_company  = '<div>';
-                                        $job_company .= '<p>'.__('The company','alven').'</p>';
+                                        $job_company .= '<p class="title-small">'.__('The company','alven').'</p>';
                                         $job_company .= '<img src="'.$company_datas['logo_url'].'" alt="'.$company_datas['name'].'">';
-                                        $job_company .= '<p>'.__('Sectors','alven').'</p>';
+                                        $job_company .= '<p class="title-small">'.__('Sectors','alven').'</p>';
                                         if( !empty($company_datas['sectors']) ):
                                             $job_company .= '<ul>';
                                             foreach ($company_datas['sectors'] as $key => $value) {
