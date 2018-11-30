99c8da63331d6256107db5951adbc28f2efe2773
Charles <charles@chiot.home>
2018-11-30 16:26:02 +0100

    Update single job template

--- wp/wp-content/themes/alven/careers.php
+++ wp/wp-content/themes/alven/careers.php
@@ -46,7 +46,7 @@ function extend_datas( $s, $k ){
     return $extended;
 }
 
-function checkParams(){
+function check_params(){
     global $params;
     global $taxquery;
     global $metquery;
@@ -65,6 +65,20 @@ function checkParams(){
     }
 }
 
+function extend_post( $id ){
+    $company_datas = null;
+    
+    if( $id ):
+        $company_datas = array(
+            'name'     => get_the_title( $id ),
+            'logo_url' => get_the_post_thumbnail_url( $id ),
+            'sectors'  => wp_get_post_terms( $id, 'field' )
+        );
+    endif;
+
+    return $company_datas;
+}
+
 
 get_header();
 
@@ -75,9 +89,15 @@ get_header();
             $is_details = is_singular( 'job' );
 
             if( $is_details ):
-            else:
-                checkParams();
 
+                $current_id = get_the_ID();
+
+                $details       = get_field('job_details', $current_id);
+                $urlapply      = get_field('job_link', $current_id);
+                $company_datas = extend_post( get_field('job_company', $current_id) );
+
+            else:
+                check_params();
 
                 // Get locations
                 $locations = get_terms( array(
@@ -222,8 +242,59 @@ get_header();
                     <div class='col-8 no-padding-right'>
                         <?php 
                             if( $is_details ):
-                                echo 'contenu de loffre';
-                                var_dump(get_field('job_location', get_the_ID()));
+                                echo '<div class="details-job flex-container">';
+                                    echo '<div class="details-job-main">';
+                                        $details_header  = '<header>';
+                                        $details_header .= '<div><p>'.__('Job Type','alven').'</p><p>Permanent Contract</p></div>';
+                                        $details_header .= '<div><p>'.__('Location','alven').'</p><p>Paris</p></div>';
+                                        $details_header .= '</header>';
+                                        echo $details_header;
+
+                                        $details_section = '<section>';
+                                        if( $details ):
+                                            if( $details['job_details_about'] ):
+                                                $details_section .= '<h2>'.__('About','alven').'&nbsp'.$company_datas['name'].'</h2>';
+                                                $details_section .= $details['job_details_about'];
+                                            endif;
+                                            if( $details['job_details_desc'] ):
+                                                $details_section .= '<h2>'.__('Job description','alven').'</h2>';
+                                                $details_section .= $details['job_details_desc'];
+                                            endif;
+                                            if( $details['job_details_profil'] ):
+                                                $details_section .= '<h2>'.__('Profile','alven').'</h2>';
+                                                $details_section .= $details['job_details_profil'];
+                                            endif;
+                                            if( $details['job_details_why'] ):
+                                                $details_section .= '<h2>'.__('Why you should join us','alven').'</h2>';
+                                                $details_section .= $details['job_details_why'];
+                                            endif;
+                                            if( $details['job_details_process'] ):
+                                                $details_section .= '<h2>'.__('Hiring Process','alven').'</h2>';
+                                                $details_section .= $details['job_details_process'];
+                                            endif;
+                                        endif;
+                                        $details_section .= '</section>';
+                                        echo $details_section;
+                                    echo '</div>';
+                                    echo '<div class="details-job-sidebar">';
+                                        if( $urlapply ):
+                                            echo '<a href="'.$urlapply.'" alt="'.__('Apply','alven').'" tarrget="_blank" rel="noopener noreferrer">'.__('Apply','alven').'</a>';
+                                        endif;
+                                        $job_company  = '<div>';
+                                        $job_company .= '<p>'.__('The company','alven').'</p>';
+                                        $job_company .= '<img src="'.$company_datas['logo_url'].'" alt="'.$company_datas['name'].'">';
+                                        $job_company .= '<p>'.__('Sectors','alven').'</p>';
+                                        if( !empty($company_datas['sectors']) ):
+                                            $job_company .= '<ul>';
+                                            foreach ($company_datas['sectors'] as $key => $value) {
+                                                $job_company .= '<li>'.$value->name.'</li>';
+                                            }
+                                            $job_company .= '</ul>';
+                                        endif;
+                                        $job_company  .= '</div>';
+                                        echo $job_company;
+                                    echo '</div>';
+                                echo '</div>';
                             else:
                                 echo '<div class="list-jobs flex-container">';
                                 if( !empty($jobs_extended) ):
