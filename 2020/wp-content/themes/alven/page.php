9a9abf3712c198d84ff243a1feb27c0cede76772
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-04-13 15:42:15 +0200

    💄 header

--- 2020/wp-content/themes/alven/page.php
+++ 2020/wp-content/themes/alven/page.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
+<div class='title-wrapper'>
+	<div class='container'>
+		<h1><?php the_title(); ?></h1>
+	</div>
+</div>
+
 <div class='container'>
 
 	<?php if ( have_posts() ) : the_post(); ?>
 
-		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	
-	<?php else : ?>
-				
-		<h1>404</h1>
-
 	<?php endif; ?>
 
 </div>
