f74a01c16968d058ca46f676f1929d977e93ec03
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-04-21 15:40:19 +0200

    💄 forms

--- 2020/wp-content/themes/alven/includes/contact.php
+++ 2020/wp-content/themes/alven/includes/contact.php
@@ -28,38 +28,30 @@
             <p class='form-success'>Your message has been sent!<br> Thank you, we will be back to you shortly.</p>
         <?php }else{ ?>
             <button class='btn open-form'>Contact us</button>
-            <form action='<?php the_permalink(); ?>#contact' method='post' class='align-left form-to-open <?php if($status2 === "error") echo "form-open-error"; ?>'>
-                <fieldset>
-                    <legend class='active'>Please <span>Introduce yourself</span></legend>
-                    <section class='form-section <?php if($errorFirstname2 || $errorLastname2 || $errorEmail2) echo "invalid"; ?>'>
-                        <div>
-                            <input type='text' name='firstname2' id='firstname2' required class='form-elt <?php if($errorFirstname2) echo "invalid"; ?>' value='<?php echo $firstname2; ?>'>
-                            <label for='firstname2'>Your first name</label>
-                        </div>
-                        <div>
-                            <input type='text' name='lastname2' id='lastname2' required class='form-elt <?php if($errorLastname2) echo "invalid"; ?>' value='<?php echo $lastname2; ?>'>
-                            <label for='lastname2'>Your last name</label>
-                        </div>
-                        <div class='large'>
-                            <input type='email' name='email2' id='email2' required class='form-elt <?php if($errorEmail2) echo "invalid"; ?>' value='<?php echo $email2; ?>'>
-                            <label for='email2'>Your email</label>
-                        </div>
-                        <div class='hidden'>
-                            <input type='url' name='url2' id='url2' value='<?php echo $spamUrl; ?>'>
-                            <label for='url2'>Leave this field empty please</label>
-                        </div>
-                    </section>
-                </fieldset>
-                <fieldset>
-                    <legend>Your message <span>How can we help you? </span></legend>
-                    <section class='form-section <?php if($errorMsg2) echo "invalid"; ?>'>
-                        <div class='full'>
-                            <textarea id='msg' name='msg' required class='form-elt <?php if($errorMsg2) echo "invalid"; ?>'><?php echo $msg2; ?></textarea>
-                            <label for='msg'>Write your inquiry here</label>
-                        </div>
-                    </section>
-                </fieldset>
-                <button type='submit' name='submitcontact' class='btn-invert'>Confirm</button>
+            <form action='<?php the_permalink(); ?>#contact' method='post' class='form-to-open <?php if($status2 === "error") echo "form-open-error"; ?>'>
+                <div class='field-wrapper'>
+                    <div class='field'>
+                        <input type='text' name='firstname2' id='firstname2' required class='form-elt <?php if($errorFirstname2) echo "invalid"; ?>' value='<?php echo $firstname2; ?>'>
+                        <label for='firstname2'>First name</label>
+                    </div>
+                    <div class='field'>
+                        <input type='text' name='lastname2' id='lastname2' required class='form-elt <?php if($errorLastname2) echo "invalid"; ?>' value='<?php echo $lastname2; ?>'>
+                        <label for='lastname2'>Last name</label>
+                    </div>
+                </div>
+                <div class='field'>
+                    <input type='email' name='email2' id='email2' required class='form-elt <?php if($errorEmail2) echo "invalid"; ?>' value='<?php echo $email2; ?>'>
+                    <label for='email2'>Email</label>
+                </div>
+                <div class='hidden'>
+                    <input type='url' name='url2' id='url2' value='<?php echo $spamUrl; ?>'>
+                    <label for='url2'>Leave this field empty please</label>
+                </div>
+                <div class='field <?php if($errorMsg2) echo "invalid"; ?>'>
+                    <textarea id='msg' name='msg' required class='form-elt <?php if($errorMsg2) echo "invalid"; ?>'><?php echo $msg2; ?></textarea>
+                    <label for='msg'>Message</label>
+                </div>
+                <button type='submit' name='submitcontact' class='btn'>Confirm</button>
             </form>
         <?php } ?>
     </div>
