eefe783c6879c1a22c674f773f0ae94448d5d760
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-04-22 11:03:32 +0200

    💄 history

--- 2020/wp-content/themes/alven/about.php
+++ 2020/wp-content/themes/alven/about.php
@@ -29,17 +29,34 @@ get_header(); ?>
                     }
                 ?>
 			</ul>
-			<ul class='history'>
-				<?php
-					$datesQuery = new WP_Query(array('post_type' => 'date', 'posts_per_page' => -1));
-					if( $datesQuery->have_posts() ) : while( $datesQuery->have_posts() ) : $datesQuery->the_post();
-				?>
-					<li class='history-date <?php the_field('bg'); ?>'>
-						<h3><?php the_title(); ?></h3>
-						<?php the_content(); ?>
-					</li>
-				<?php endwhile; endif; ?>
-			</ul>
+			<div class='history'>
+				<ul class='dates clearfix'>
+					<?php
+						$datesQuery = new WP_Query(array('post_type' => 'date', 'posts_per_page' => -1));
+						if( $datesQuery->have_posts() ) : while( $datesQuery->have_posts() ) : $datesQuery->the_post();
+							$cover = get_field('cover');
+							$hasImg = has_post_thumbnail();
+							$link = get_field('link');
+					?>
+						<li class='history-date <?php the_field('bg'); if( $hasImg && !$cover ) echo " has-icon";?>'>
+							<?php if( $hasImg ) : ?>
+								<div class="img" <?php if( $cover ) echo 'style="background-image:url(' . get_the_post_thumbnail_url($post, 'medium') . ')"'; ?>>
+									<?php if( !$cover ) the_post_thumbnail('medium'); ?>
+								</div>
+							<?php endif; ?>
+							<div class="txt">
+								<h3><?php the_title(); ?></h3>
+								<?php the_content(); ?>
+								<?php if( $link ) : ?>
+									<p>
+										<a href='<?php echo $link['url']; ?>' class='link-roboto'><?php echo $link['title']; ?></a>
+									</p>
+								<?php endif; ?>
+							</div>
+						</li>
+					<?php endwhile; endif; ?>
+				</ul>
+			</div>
 		</div>
 	</div>
 	
