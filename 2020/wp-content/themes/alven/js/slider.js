966044f08634550857044a0155744f1c2c22136d
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-05-05 12:37:09 +0200

    ✨ slider auto

--- 2020/wp-content/themes/alven/js/slider.js
+++ 2020/wp-content/themes/alven/js/slider.js
@@ -9,6 +9,13 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var gsap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! gsap */ "./node_modules/gsap/index.js");
+/* harmony import */ var gsap_ScrollToPlugin_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! gsap/ScrollToPlugin.js */ "./node_modules/gsap/ScrollToPlugin.js");
+
+
+
+gsap__WEBPACK_IMPORTED_MODULE_0__["gsap"].registerPlugin(gsap_ScrollToPlugin_js__WEBPACK_IMPORTED_MODULE_1__["ScrollToPlugin"]);
+
 const slider = () => {
     const slider = document.getElementById('slider');
     const nav = document.getElementById('slider-nav');
@@ -17,21 +24,40 @@ const slider = () => {
 
     const slides = slider.querySelectorAll('.slide');
     const btns = nav.querySelectorAll('button');
+    let index = 0;
+    let nextBtn;
+    let slideAutoCall;
+
+    const slide = btn => {
+        slideAutoCall.kill();
+
+        slides.forEach(slide => {
+            slide.classList.remove('on');
+        });
+        slider.querySelector('[data-startup="' + btn.dataset.slide + '"').classList.add('on');
+
+        btns.forEach(elt => {
+            elt.classList.remove('on');
+        });
+        btn.classList.add('on');
+        btn.blur();
+
+        slideAutoCall = gsap__WEBPACK_IMPORTED_MODULE_0__["gsap"].delayedCall(5, slideAuto);
+    };
+
+    const slideAuto = () => {
+        index = [].indexOf.call(btns, nav.querySelector('.on'));
+        nextBtn = btns[index + 1] ? btns[index + 1] : btns[0];
+        slide(nextBtn);
+    };
+
+    slideAutoCall = gsap__WEBPACK_IMPORTED_MODULE_0__["gsap"].delayedCall(5, slideAuto);
 
     btns.forEach(btn => {
         btn.addEventListener(
             'click',
             () => {
-                slides.forEach(slide => {
-                    slide.classList.remove('on');
-                });
-                slider.querySelector('[data-startup="' + btn.dataset.slide + '"').classList.add('on');
-
-                btns.forEach(elt => {
-                    elt.classList.remove('on');
-                });
-                btn.classList.add('on');
-                btn.blur();
+                slide(btn);
             },
             false
         );
@@ -44,4 +70,4 @@ const slider = () => {
 /***/ })
 
 }]);
-//# sourceMappingURL=slider.js.map?bf6e07f1596c9325f9eb89ec5a61d5b0
+//# sourceMappingURL=slider.js.map?1b7dfe94fd3ec6b02e7fd78fb3175c50
--- 2020/wp-content/themes/alven/src/js/components/slider.js
+++ 2020/wp-content/themes/alven/src/js/components/slider.js
@@ -1,3 +1,8 @@
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin.js';
+
+gsap.registerPlugin(ScrollToPlugin);
+
 const slider = () => {
     const slider = document.getElementById('slider');
     const nav = document.getElementById('slider-nav');
@@ -6,21 +11,40 @@ const slider = () => {
 
     const slides = slider.querySelectorAll('.slide');
     const btns = nav.querySelectorAll('button');
+    let index = 0;
+    let nextBtn;
+    let slideAutoCall;
+
+    const slide = btn => {
+        slideAutoCall.kill();
+
+        slides.forEach(slide => {
+            slide.classList.remove('on');
+        });
+        slider.querySelector('[data-startup="' + btn.dataset.slide + '"').classList.add('on');
+
+        btns.forEach(elt => {
+            elt.classList.remove('on');
+        });
+        btn.classList.add('on');
+        btn.blur();
+
+        slideAutoCall = gsap.delayedCall(5, slideAuto);
+    };
+
+    const slideAuto = () => {
+        index = [].indexOf.call(btns, nav.querySelector('.on'));
+        nextBtn = btns[index + 1] ? btns[index + 1] : btns[0];
+        slide(nextBtn);
+    };
+
+    slideAutoCall = gsap.delayedCall(5, slideAuto);
 
     btns.forEach(btn => {
         btn.addEventListener(
             'click',
             () => {
-                slides.forEach(slide => {
-                    slide.classList.remove('on');
-                });
-                slider.querySelector('[data-startup="' + btn.dataset.slide + '"').classList.add('on');
-
-                btns.forEach(elt => {
-                    elt.classList.remove('on');
-                });
-                btn.classList.add('on');
-                btn.blur();
+                slide(btn);
             },
             false
         );
